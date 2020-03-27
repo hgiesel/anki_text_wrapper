0a19ee8af7816069e7da42260f580d6988a65a65
Henrik Giesel <hengiesel@gmail.com>
2020-03-27 10:24:55 +0100

    Improve link script

--- link.sh
+++ link.sh
@@ -4,33 +4,22 @@ declare customdir=''
 
 declare projname='TextWrapperDev'
 
-if [[ "$1" =~ ^d ]]; then
-  if [[ "$customdir" ]]; then
-    rm "${customdir}/${projname}"
+if [[ "$customdir" ]]; then
+  target="${customdir}/${projname}"
 
-  elif [[ $(uname) = 'Darwin' ]]; then
-    rm ~"/Library/Application\ Support/Anki2/addons21/${projname}"
+elif [[ $(uname) = 'Darwin' ]]; then
+  target="$HOME/Library/Application Support/Anki2/addons21/${projname}"
 
-  elif [[ $(uname) = 'Linux' ]]; then
-    rm ~"/.local/share/Anki2/addons21/${projname}"
-
-  else
-    echo 'Unknown platform'
-    exit -1
-  fi
+elif [[ $(uname) = 'Linux' ]]; then
+  target="$HOME/.local/share/Anki2/addons21/${projname}"
 
 else
-  if [[ $customdir ]]; then
-    ln -s "${DIR}" "${customdir}/${projname}"
+  echo 'Unknown platform'
+  exit -1
+fi
 
-  elif [[ $(uname) = 'Darwin' ]]; then
-    ln -s "${DIR}" ~"/Library/Application Support/Anki2/addons21/${projname}"
-
-  elif [[ $(uname) = 'Linux' ]]; then
-    ln -s "${DIR}" ~"/.local/share/Anki2/addons21/${projname}"
-
-  else
-    echo 'Unknown platform'
-    exit -1
-  fi
+if [[ "$1" == '-d' || "$1" =~ ^d ]]; then
+  rm "$target"
+elif [[ ! -h "${target}" ]]; then
+  ln -s "${DIR}" "${target}"
 fi
